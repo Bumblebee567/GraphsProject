982bce1cc79fcce35642735153e49f7ac065483b
Admin <Admin@DESKTOP-F2B4VCM>
2017-12-17 20:15:21 +0100

    Generating random Brush.Color

---
+++ WpfApp1/Utiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    class Utiles
+    {
+        private Brush[] GenerateRandomBrushes(int size)
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            var indexes = Enumerable.Range(0, size).OrderBy(Guid => new Guid());
+            var indexesArray = indexes.ToArray();
+            Brush[] results = new Brush[size];
+            for (int i = 0; i < size; i++)
+            {
+                results[i] = (Brush)properties[indexesArray[i]].GetValue(null, null);
+            }  
+
+            return results;
+        }
+    }
+}
